f0f8c84f59b78625067311cb1f7a3aefa560655a
lengcaiyoong <46414289+lengcaiyoong@users.noreply.github.com>
2023-04-27 19:15:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ fetch('textdetails.txt')
   .then(response => response.text())
   .then(data => {
     // Parse the text content into an array
-    const dataArray = data.split(',');
+    const dataArray = data.split('\n').map(row => row.split(', '));
 
     // Create a table element
     const table = document.getElementById('my-table');
@@ -11,15 +11,22 @@ fetch('textdetails.txt')
     // Add rows and cells to the table
     for (let i = 0; i < dataArray.length; i++) {
       const row = table.insertRow();
-      const cell = row.insertCell();
-      cell.innerText = dataArray[i];
-    }
 
-    // Add a click event listener to the button
-    const copyBtn = document.getElementById('copy-btn');
-    copyBtn.addEventListener('click', () => {
-      const tableData = table.innerText;
-      navigator.clipboard.writeText(tableData);
-      alert('Table data copied to clipboard!');
-    });
+      // Add a button cell to the first column
+      const buttonCell = row.insertCell();
+      const copyBtn = document.createElement('button');
+      copyBtn.innerText = 'Copy';
+      copyBtn.addEventListener('click', () => {
+        const rowData = dataArray[i].join('\t');
+        navigator.clipboard.writeText(rowData);
+        alert('Row data copied to clipboard!');
+      });
+      buttonCell.appendChild(copyBtn);
+
+      // Add the remaining data cells to the row
+      for (let j = 0; j < 3; j++) {
+        const cell = row.insertCell();
+        cell.innerText = dataArray[i][j];
+      }
+    }
   });
