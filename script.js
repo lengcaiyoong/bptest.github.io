07d07171378782af6759974c4e8250ebef77f737
lengcaiyoong <46414289+lengcaiyoong@users.noreply.github.com>
2023-04-27 19:05:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
+// Fetch the contents of the text file
 fetch('textdetails.txt')
   .then(response => response.text())
   .then(data => {
-    document.getElementById('file-content').innerText = data;
+    // Parse the text content into an array
+    const dataArray = data.split(',');
+
+    // Create a table element
+    const table = document.getElementById('my-table');
+
+    // Add rows and cells to the table
+    for (let i = 0; i < dataArray.length; i++) {
+      const row = table.insertRow();
+      const cell = row.insertCell();
+      cell.innerText = dataArray[i];
+    }
+
+    // Add a click event listener to the button
+    const copyBtn = document.getElementById('copy-btn');
+    copyBtn.addEventListener('click', () => {
+      const tableData = table.innerText;
+      navigator.clipboard.writeText(tableData);
+      alert('Table data copied to clipboard!');
+    });
   });
